2c6db7d4998d89c7086fde087fcc356a66ce67c7
楚槟夕 <cbx@hetingjun.local>
2017-12-15 13:40:35 +0800

    Initial Commit

---
+++ ForTheSquareTests/ForTheSquareTests.m
@@ -0,0 +1,39 @@
+//
+//  ForTheSquareTests.m
+//  ForTheSquareTests
+//
+//  Created by cbx on 2017/12/15.
+//  Copyright © 2017年 cbx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ForTheSquareTests : XCTestCase
+
+@end
+
+@implementation ForTheSquareTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
